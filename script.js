545bae3398e304dfa281189fb848863fc049ee93
hellolose <39977229+hellolose@users.noreply.github.com>
2020-06-12 17:30:26 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+echo "# Cookieclickerhacks" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/hellolose/Cookieclickerhacks.git
+git push -u origin master
+                Game.Earn(INFINITY);
+                Game.Cookies(INFINITY);
